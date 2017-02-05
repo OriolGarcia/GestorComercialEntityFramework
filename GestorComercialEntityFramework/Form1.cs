7b500814497b85084ac4eaf99747dd2b6692da3c
OriolGarcia <130761.clot@fje.edu>
2017-02-05 18:59:54 +0100

    merge

--- GestorComercialEntityFramework/Form1.cs
+++ GestorComercialEntityFramework/Form1.cs
@@ -205,21 +205,12 @@ namespace GestorComercialEntityFramework
         private void btAfegir_Click_1(object sender, EventArgs e)
         {
             var rows = dataGridViewProductesFactura.SelectedRows;
-            int count = rows.Count;
-            DataGridViewRow row;
+            int count = rows.Count; MessageBox.Show(count.ToString());
             if (count > 0)
                 for (int i = 0; i < count; i++)
                 {
-                    int j = 0;
-                    row = (DataGridViewRow) rows[i].Clone();
-                    foreach (DataGridViewCell cell in rows[i].Cells)
-                    {
-                        row.Cells[j].Value = cell.Value;
-                        Console.WriteLine("Cell Value: " + cell.Value);
-                        j++;
-                    }
-                    dataGridViewProductesFacturaAfegits.Rows.Add(row);
-                    dataGridViewProductesFacturaAfegits.Refresh();
+
+                    dataGridViewProductesFacturaAfegits.Rows.Add(rows[i].Clone());
                 }
         }
 
